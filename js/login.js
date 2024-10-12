acf41bb44b4a02c9c1883844c90d6489b8cb8882
masum ahmed <info.msmtechman0.@example.com>
2024-10-12 16:55:31 +0600

    added a total amount

--- js/login.js
+++ js/login.js
@@ -3,5 +3,24 @@ document.getElementById('btn-login')
     event.preventDefault();
     console.log('login button click')
     const donateNumber = document.getElementById('donate-number').value;
-    console.log(donateNumber)
+    console.log(donateNumber);
+    
+    if(donateNumber === '100'){
+        console.log('You are logged in')
+        // window.location.href = '/home.html';
+
+        const donate = document.getElementById('total-donate')
+        .innerText;
+        console.log(donate)
+
+        const addDonate = parseFloat(donateNumber);
+        const addBalance = parseFloat(donate);
+        const newBalance = addDonate + addBalance;
+        console.log(newBalance);
+
+        document.getElementById('total-donate').innerText = newBalance;
+        
+    }else{
+        alert('wrong your donate number');
+    }
 })
