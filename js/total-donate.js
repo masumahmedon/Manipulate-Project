b26abdbec61e30bbe16f4b67bdf1c0b7145d48ba
masum ahmed <info.msmtechman0.@example.com>
2024-10-12 17:05:39 +0600

    added a section total-donate2 in js

---
+++ js/total-donate.js
@@ -0,0 +1,26 @@
+document.getElementById('btn-login')
+.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('login button click')
+    const donateNumber = document.getElementById('donate-number').value;
+    console.log(donateNumber);
+    
+    if(donateNumber === '100'){
+        console.log('You are logged in')
+        // window.location.href = '/home.html';
+
+        const donate = document.getElementById('total-donate2')
+        .innerText;
+        console.log(donate)
+
+        const addDonate = parseFloat(donateNumber);
+        const addBalance = parseFloat(donate);
+        const newBalance = addDonate - addBalance;
+        console.log(newBalance);
+
+        document.getElementById('total-donate2').innerText = newBalance;
+        
+    }else{
+        alert('wrong your donate number');
+    }
+})
